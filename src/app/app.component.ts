a6c6131b05e923cd73e560f1983eaeee339ff99a
vinicius magalhaes de souza <vmsouza@localhost.localdomain>
2020-04-01 21:32:57 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Person } from './person';
+import * as faker from 'faker'
+import { Store, select } from '@ngrx/store';
+import { AppState } from './store';
+import { PersonNew, PersonAll, PersonUpdate, PersonDelete } from './store/person.actions';
+import * as fromPersonSelectors from './store/person.selectors'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngrx-basic';
+  
+  people$: Observable<Person[]>;
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.store.dispatch(new PersonAll());
+    //this.people$ = this.store.pipe(select('people'));
+    this.people$ = this.store.select(fromPersonSelectors.selectAll);
+
+  }
+
+  addNew() {
+    let person: Person = {
+      name: faker.name.findName(),
+      address: faker.address.streetAddress(),
+      city: faker.address.city(),
+      country: faker.address.country(),
+      age: Math.round( Math.random()*100),
+      _id: new Date().getMilliseconds().toString()
+    };
+    this.store.dispatch(new PersonNew({person}));
+  }
+
+  update(p: Person) {
+    const person = {
+      name: faker.name.findName(),
+      address: faker.address.streetAddress(),
+      city: faker.address.city(),
+      country: faker.address.country(),
+      age: Math.round(Math.random() * 100)
+    };
+    this.store.dispatch(new PersonUpdate({ id: p._id, changes: person }));
+  }
+
+  delete(p: Person) {
+    this.store.dispatch( new PersonDelete({id: p._id}));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { PersonComponent } from './person/person.component';
+import { StoreModule } from '@ngrx/store';
+import { appReducers } from './store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    StoreModule.forRoot( appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
